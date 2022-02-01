d94e6ab93f4541a6c3b95450e675f0f2cf46392e
vincentreynaud <vincentreynaud.de@gmail.com>
2022-02-01 17:45:26 +0100

    rearrange project layout components

--- src/components/projects/ProjectComplexLayout.tsx
+++ src/components/projects/ProjectComplexLayout.tsx
@@ -2,43 +2,72 @@ import classNames from 'classnames';
 import Gif from 'components/Gif';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React, { useState } from 'react';
+import max from 'lodash/max';
+
+const picClassNamesByIndex = {
+  0: 'order-xl-1',
+  1: 'order-xl-2',
+  2: 'order-xl-3',
+  3: 'order-xl-4',
+};
+const picClassNamesByLayout = {
+  'text-right': 'order-xl-1',
+  'text-left': 'order-xl-2',
+  2: 'order-xl-3',
+  3: 'order-xl-4',
+};
 
 export default function ProjectComplexLayout({ data, textPosition }) {
   const { title, titleGif, exhibition, location, date, description, pictures } = data;
 
   const [isTextBlurred, setIsTextBlurred] = useState(true);
 
+  const whRatios = pictures.map((pic) => {
+    const { width, height } = pic.gatsbyImageData;
+    return height - width;
+  });
+  const highestRatioPicIndex = whRatios.indexOf(max(whRatios));
+  console.log(highestRatioPicIndex);
+  const isLayoutLeft = textPosition === 'text-left';
+
   return (
     <>
       <h2 className='visually-hidden'>{title}</h2>
-      <div className='container-fluid position-relative project-layout-right'>
+      <div className='container-fluid position-relative project-layout-complex gx-0'>
         <div className='title position-absolute'>
           <Gif url={titleGif.file.url} className='w-100' />
         </div>
-        <ul className='row list-unstyled'>
-          {pictures.map((pic) => {
-            const { width, height } = pic.gatsbyImageData;
-            if (pic.file.contentType.includes('gif')) {
-              return (
-                <li className={`${width < height ? 'col-auto' : 'col'} order-2 order-xl-1`}>
-                  <Gif url={pic.file.url} alt={`${title} project animated picture`} className='pic-max-height' />
-                </li>
-              );
+        <ul className='row list-unstyled gx-2'>
+          {pictures.map((pic, i) => {
+            let classNames = picClassNamesByIndex[i];
+            if (i === highestRatioPicIndex) {
+              classNames += ' col-auto order-2';
             } else {
-              return (
-                <li className='col-12 col-xl order-1 order-xl-2'>
+              classNames += ' col-12 col-xl order-1';
+            }
+            if (!isLayoutLeft) {
+              // classNames += ' col-xl-auto';
+            }
+            return (
+              <li className={classNames}>
+                {pic.file.contentType.includes('gif') ? (
+                  <Gif url={pic.file.url} alt={`${title} project animated picture`} className='pic-max-height' />
+                ) : (
                   <GatsbyImage
                     objectFit='contain'
                     image={getImage(pic)!}
                     alt={`${title} project picture`}
                     className='pic-max-height'
                   />
-                </li>
-              );
-            }
+                )}
+              </li>
+            );
           })}
           <li
-            className='col-auto order-3'
+            className={classNames('col-auto', {
+              'order-3': !isLayoutLeft,
+              'order-0': isLayoutLeft,
+            })}
             onMouseOver={() => setIsTextBlurred(false)}
             onMouseOut={() => setIsTextBlurred(true)}
           >
--- src/components/projects/ProjectSimpleLayout.tsx
+++ src/components/projects/ProjectSimpleLayout.tsx
@@ -2,37 +2,31 @@ import Gif from 'components/Gif';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
 
-export default function ProjectBelowLayout({ data, textPosition }) {
+export default function ProjectSimpleLayout({ data, textPosition }) {
   const { title, titleGif, exhibition, location, date, description, pictures } = data;
-  console.log(data);
   return (
     <>
       <h2 className='visually-hidden'>{title}</h2>
-      <div className='container-fluid position-relative project-layout-below'>
+      <div className='container-fluid position-relative project-layout-simple'>
         <div className='title position-absolute'>
           <Gif url={titleGif.file.url} className='w-100' />
         </div>
         <ul className='row list-unstyled'>
           {pictures.map((pic) => {
-            const { width, height } = pic.gatsbyImageData;
-            if (pic.file.contentType.includes('gif')) {
-              return (
-                <li className={`${width < height ? 'col-auto' : 'col'} order-2 order-xl-1`}>
+            return (
+              <li className='col-12 col-xl order-1 order-xl-2'>
+                {pic.file.contentType.includes('gif') ? (
                   <Gif url={pic.file.url} alt={`${title} project animated picture`} className='pic-max-height' />
-                </li>
-              );
-            } else {
-              return (
-                <li className='col-12 col-xl order-1 order-xl-2'>
+                ) : (
                   <GatsbyImage
                     objectFit='contain'
                     image={getImage(pic)!}
                     alt={`${title} project picture`}
                     className='pic-max-height'
                   />
-                </li>
-              );
-            }
+                )}
+              </li>
+            );
           })}
           <li className='col order-3'>
             <div className='description-max-width'>
