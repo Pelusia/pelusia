46e992e03cffc95b588f3a427b530eab9b96cac1
vincentreynaud <vincentreynaud.de@gmail.com>
2022-02-03 21:51:37 +0100

    lazyload gifs

--- src/components/Gif.tsx
+++ src/components/Gif.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Lazyload from 'react-lazyload';
 
 type Props = {
   className?: string;
   alt?: string;
   url: string;
+  height?: number;
 };
 
-export default function Gif({ className, alt, url }: Props) {
-  return <img src={`https:${url}`} alt={alt} className={className} />;
+export default function Gif({ className, alt, url, height }: Props) {
+  return (
+    <Lazyload height={height} offset={600} once>
+      <img src={`https:${url}`} alt={alt} className={className} />
+    </Lazyload>
+  );
 }
