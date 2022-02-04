da160d3d53c2206208ccf772e1073e92da8a9206
Vincent Reynaud <vincentreynaud.de@gmail.com>
2022-02-04 16:39:05 +0100

    add layout margins + finish navigation function

--- src/components/Gif.tsx
+++ src/components/Gif.tsx
@@ -10,8 +10,8 @@ type Props = {
 
 export default function Gif({ className, alt, url, height }: Props) {
   return (
-    <Lazyload height={height} offset={600} once>
-      <img src={`https:${url}`} alt={alt} className={className} />
+    <Lazyload className={className} height={height} offset={600} once>
+      <img src={`https:${url}`} alt={alt} className='w-100 h-100' />
     </Lazyload>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import bubble from '../images/info-sm.png';
 import InfoBubble from './InfoBubble';
+import classNames from 'classnames';
 
 export default function Navigation({ brand, links, location, bio }) {
   const [infoIsShown, setInfoIsShown] = useState(false);
+  const isHomePage = location?.pathname === '/';
 
   const showInfoBubble = (boo: boolean) => {
     console.log('show trigger');
     if (infoIsShown === boo) {
+      return;
+    } else if (!isHomePage) {
+      setInfoIsShown(false);
       return;
     }
     setInfoIsShown(boo);
@@ -16,9 +21,21 @@ export default function Navigation({ brand, links, location, bio }) {
 
   return (
     <div className='d-inline' onMouseOver={() => showInfoBubble(true)} onMouseOut={() => showInfoBubble(false)}>
-      <Link to={location?.pathname === '/' ? 'info' : '/'} id='navigation' className='position-fixed p-0'>
-        <img src={bubble} alt='' className='info-link d-inline' />
-      </Link>
+      {isHomePage ? (
+        <div
+          id='navigation'
+          className={classNames('position-fixed p-0', {
+            'fade-out': infoIsShown,
+            'fade-in': !infoIsShown,
+          })}
+        >
+          <img src={bubble} alt='' className='info-link d-inline' />
+        </div>
+      ) : (
+        <Link to='/' id='navigation' className='position-fixed p-0'>
+          <img src={bubble} alt='' className='info-link d-inline' />
+        </Link>
+      )}
       <InfoBubble brand={brand} links={links} bio={bio} isVisible={infoIsShown} />
     </div>
   );
